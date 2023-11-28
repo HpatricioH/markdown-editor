5e9bb423586bd5a8887eafc4dfcded8592936db2
HPHuerta <hhernanp@gmail.com>
2023-11-28 01:41:11 -0500

    Add authentication options and database configuration

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,8 @@
-// we can import authOptions to setup the handler from NextAuth
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { authOptions } from '@/auth/authOptions'
+import { type NextAuthOptions } from 'next-auth'
+import NextAuth from 'next-auth/next'
+
+const handler = NextAuth(authOptions as NextAuthOptions)
+
+export { handler as GET, handler as POST }
--- src/auth/authOptions.ts
+++ src/auth/authOptions.ts
@@ -1,1 +1,46 @@
-// auth options to be added here for nextAuth.
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import NextAuth from 'next-auth'
+import { type NextAuthOptions } from 'next-auth'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import prisma from '@/app/lib/db/db'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt'
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID?.toString() ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET?.toString() ?? ''
+    })
+  ],
+  callbacks: {
+    // We can pass in additional information from the user document MongoDB returns
+    async jwt ({ token, user }: any) {
+      if (user) {
+        token.user = {
+          id: user.id as string,
+          email: user.email as string,
+          name: user.name as string,
+          image: user.image as string
+        }
+      }
+      return token
+    },
+    // If we want to access our extra user info from sessions we have to pass it the token here to get them in sync:
+    session: async ({ session, token }: any) => {
+      if (token) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        session.user = token.user
+      }
+      return session
+    }
+  }
+}
+
+export default NextAuth(authOptions)

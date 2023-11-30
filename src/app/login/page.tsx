116b7e74d065aaa68b6d55d2c32ea0a751267836
HPHuerta <hhernanp@gmail.com>
2023-11-30 01:28:27 -0500

    Login UI form created

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from './form'
 
 export default function LoginPage () {
   return (
-    <section>
+    <section className="bg-gradient-to-br from-[#151619] to-[#2B2D31] h-screen w-screen flex items-center justify-center overflow-y-hidden">
       <LoginForm />
     </section>
   )

5570779036dd5ebe1da583b3d00cb3d844219322
HPHuerta <hhernanp@gmail.com>
2023-12-06 00:49:19 -0500

    Add header navigation icons and styles

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,11 +1,44 @@
 'use client'
 
 import { signOut } from 'next-auth/react'
+import Image from 'next/image'
 
 export default function Header () {
   return (
-    <header>
-      <button onClick={() => { signOut() } }>Sign out</button>
+    <header className="fixed left-0 top-0 w-full bg-[#35393F]">
+      <nav className='flex flex-wrap gap-4 justify-around items-baseline '>
+        <div className='flex-grow-1 bg-[#7C8187] pb-6 pt-8'>
+          <Image
+            src={'/icons/icon-menu.svg'}
+            alt='Menu Icon'
+            width={24}
+            height={24}
+          />
+        </div>
+        <Image
+          src={'/icons/icon-document.svg'}
+          alt='Document Icon'
+          width={24}
+          height={24}
+          className='flex-grow-1'
+        />
+        <p>Welcome.md</p>
+        <Image
+          src={'/icons/icon-delete.svg'}
+          alt='Delete Icon'
+          width={24}
+          height={24}
+          className='flex-grow-1'
+        />
+        <Image
+          src={'/icons/icon-save.svg'}
+          alt='Save Icon'
+          width={24}
+          height={24}
+          className='flex-grow-1'
+        />
+        <button onClick={() => { signOut() } }>Sign out</button>
+      </nav>
     </header>
   )
 }

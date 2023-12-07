44b30f6d245cc74840d69f6457905ec01d2e6d43
HPHuerta <hhernanp@gmail.com>
2023-12-06 23:05:02 -0500

    Update Header component styling

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,43 +1,49 @@
 'use client'
 
-import { signOut } from 'next-auth/react'
 import Image from 'next/image'
 
 export default function Header () {
   return (
-    <header className="fixed left-0 top-0 w-full bg-[#35393F]">
-      <nav className='flex flex-wrap gap-4 justify-around items-baseline '>
-        <div className='flex-grow-1 bg-[#7C8187] pb-6 pt-8'>
+    <header className="fixed left-0 top-0 w-full flex justify-around bg-[#2B2D31] text-white">
+      <div className='flex-grow-1 flex justify-center items-center bg-[#35393F] pb-8 pt-8 w-[3.9rem]'>
+        <Image
+          src={'/icons/icon-menu.svg'}
+          alt='Menu Icon'
+          width={24}
+          height={24}
+        />
+      </div>
+      <nav className='flex flex-1 flex-wrap gap-4 justify-between items-center px-5'>
+        <div className='flex gap-4'>
           <Image
-            src={'/icons/icon-menu.svg'}
-            alt='Menu Icon'
+            src={'/icons/icon-document.svg'}
+            alt='Document Icon'
             width={24}
             height={24}
+            className='flex-grow-1'
           />
+          <p className='lowercase'>Welcome.md</p>
         </div>
-        <Image
-          src={'/icons/icon-document.svg'}
-          alt='Document Icon'
-          width={24}
-          height={24}
-          className='flex-grow-1'
-        />
-        <p>Welcome.md</p>
-        <Image
-          src={'/icons/icon-delete.svg'}
-          alt='Delete Icon'
-          width={24}
-          height={24}
-          className='flex-grow-1'
-        />
-        <Image
-          src={'/icons/icon-save.svg'}
-          alt='Save Icon'
-          width={24}
-          height={24}
-          className='flex-grow-1'
-        />
-        <button onClick={() => { signOut() } }>Sign out</button>
+        <div className='flex gap-2'>
+          <div className='p-4'>
+            <Image
+              src={'/icons/icon-delete.svg'}
+              alt='Delete Icon'
+              width={24}
+              height={24}
+              className='flex-grow-1'
+            />
+          </div>
+          <div className='bg-[#E46643] rounded-md p-4'>
+            <Image
+              src={'/icons/icon-save.svg'}
+              alt='Save Icon'
+              width={24}
+              height={24}
+              className='flex-grow-1'
+            />
+          </div>
+        </div>
       </nav>
     </header>
   )

89baa144a6fe27f5a44319b84ed371a8c5cb2e4f
HPHuerta <hhernanp@gmail.com>
2023-12-08 01:23:26 -0500

    Update Header and Page components

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,42 +4,42 @@ import Image from 'next/image'
 
 export default function Header () {
   return (
-    <header className="fixed left-0 top-0 w-full flex justify-around bg-[#2B2D31] text-white">
-      <div className='flex-grow-1 flex justify-center items-center bg-[#35393F] pb-8 pt-8 w-[3.9rem]'>
+    <header className="fixed left-0 top-0 w-full flex justify-around bg-[#2B2D31] text-white text-sm">
+      <div className='flex-grow-1 flex justify-center items-center bg-[#35393F] pb-6 pt-6 w-[3.9rem]'>
         <Image
           src={'/icons/icon-menu.svg'}
           alt='Menu Icon'
-          width={24}
-          height={24}
+          width={18}
+          height={18}
         />
       </div>
-      <nav className='flex flex-1 flex-wrap gap-4 justify-between items-center px-5'>
-        <div className='flex gap-4'>
+      <nav className='flex flex-1 flex-wrap gap-2 justify-between items-center px-2'>
+        <div className='flex gap-2'>
           <Image
             src={'/icons/icon-document.svg'}
             alt='Document Icon'
-            width={24}
-            height={24}
+            width={18}
+            height={18}
             className='flex-grow-1'
           />
           <p className='lowercase'>Welcome.md</p>
         </div>
-        <div className='flex gap-2'>
-          <div className='p-4'>
+        <div className='flex'>
+          <div className='p-3'>
             <Image
               src={'/icons/icon-delete.svg'}
               alt='Delete Icon'
-              width={24}
-              height={24}
+              width={18}
+              height={18}
               className='flex-grow-1'
             />
           </div>
-          <div className='bg-[#E46643] rounded-md p-4'>
+          <div className='bg-[#E46643] rounded-md p-3'>
             <Image
               src={'/icons/icon-save.svg'}
               alt='Save Icon'
-              width={24}
-              height={24}
+              width={18}
+              height={18}
               className='flex-grow-1'
             />
           </div>

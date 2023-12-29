88dbaddf0a7d86bc08b670a265b541386b87142d
HPHuerta <hhernanp@gmail.com>
2023-12-28 23:42:14 -0500

    Refactor MarkDownEditor component and add MarkDownResult component

--- src/app/components/MarkDownEditor/MarkDownEditor.tsx
+++ src/app/components/MarkDownEditor/MarkDownEditor.tsx
@@ -1,18 +1,18 @@
 'use client'
-import ReactMarkdown from 'react-markdown'
 import 'easymde/dist/easymde.min.css'
-import { useState } from 'react'
+import { useEditorInput } from '@/app/lib/store/useEditorInput'
 
 export default function MarkDownEditor () {
-  const [markdown, setMarkdown] = useState('# Type your markdown here')
+  const { setMarkdownInput } = useEditorInput()
 
   return (
     <section className="flex">
-      <div className="w-1/2">
-        <textarea name="" id="" cols={39} rows={30.5} onChange={(event) => { setMarkdown(event.target.value) }} className='bg-[#151619] text-[#FFF] focus:outline-0'></textarea>
-      </div>
-      <div className="w-1/2 bg-[#151619] text-[#FFF]">
-        <ReactMarkdown>{markdown}</ReactMarkdown>
+      <div className="w-full">
+        <textarea
+          autoFocus
+          onChange={(event) => { setMarkdownInput(event.target.value) }}
+          className='dark:bg-[#151619] dark:text-[#FFF] focus:outline-0 text-[1rem] h-screen w-full p-2'>
+        </textarea>
       </div>
     </section>
   )

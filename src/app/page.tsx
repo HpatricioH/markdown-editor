6efa3dc23f971050b376814d081369ad225905d5
HPHuerta <hhernanp@gmail.com>
2023-12-28 23:41:53 -0500

    Remove SignOutButton component and update flex layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import SignOutButton from '@/core/utils/SignOutButton'
 import VisibilityBar from './components/VisibilityBar/VisibilityBar'
 import MarkDownEditor from './components/MarkDownEditor/MarkDownEditor'
 
@@ -6,7 +5,7 @@ export default async function Home () {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-[3.7rem]">
       <VisibilityBar />
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex lg:flex-col">
         {/* <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -27,9 +26,6 @@ export default async function Home () {
         </div> */}
         <MarkDownEditor />
 
-        <div className=" ">
-          <SignOutButton/>
-        </div>
       </div>
 
     </main>

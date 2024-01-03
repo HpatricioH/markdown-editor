a7bb1d6734848b6020576ff0066fbd58ab764111
HPHuerta <hhernanp@gmail.com>
2024-01-02 23:44:45 -0500

    Add MarkDownEditor component and fonts module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,7 @@ export default async function Home () {
             />
           </a>
         </div> */}
+
         <MarkDownEditor />
 
       </div>
---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Roboto_Slab, Roboto } from 'next/font/google'
+
+export const robotoSlab = Roboto_Slab({ subsets: ['latin'], weight: ['400', '700', '900'] })
+
+export const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700', '900'] })

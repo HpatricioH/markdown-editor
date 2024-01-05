aff9b33cce79402021392e33e4669972d4fbd37c
HPHuerta <hhernanp@gmail.com>
2024-01-05 00:36:02 -0500

    Update Sidebar and VisibilityBar styles

--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,8 @@
 export default function Sidebar () {
   return (
-    <aside className='flex flex-col justify-center items-center h-screen w-full'>
-        <div className='flex flex-col justify-center items-center w-full'>
-          <h1 className='text-3xl font-bold text-gray-800 dark:text-gray-100'>MarkDown Editor</h1>
-        </div>
-   </aside>
+    <aside id="sidebar"
+    className="fixed top-0 -left-0 w-[250px] bg-dark-gray-3 h-screen px-6 py-7 transition-all">
+        <h1 className='text-sm font-bold text-gray-800 dark:text-gray-100'>MarkDown Editor</h1>
+    </aside>
   )
 }

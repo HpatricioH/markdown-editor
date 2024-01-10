651d4825d583120b0527f94f594619f7ce47b039
HPHuerta <hhernanp@gmail.com>
2024-01-10 00:32:39 -0500

    Add onClick prop to Button component

--- src/core/utils/Button.tsx
+++ src/core/utils/Button.tsx
@@ -1,6 +1,7 @@
 interface ButtonProps {
   children: React.ReactNode
   className?: string
+  onClick?: () => void
 }
 
 export default function Button ({ children, className, ...props }: ButtonProps) {

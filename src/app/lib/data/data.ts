03a173eb64a3332a24af6669dfdc6cbe252872f1
HPHuerta <hhernanp@gmail.com>
2024-01-17 23:08:08 -0500

    Refactor Documents component and fetchDocuments function

--- src/app/lib/data/data.ts
+++ src/app/lib/data/data.ts
@@ -1,5 +1,6 @@
-interface DocumentProps {
+export interface DocumentProps {
   name?: string
+  createdAt?: string
   content?: string
   userId: string
   id?: string
@@ -7,10 +8,9 @@ interface DocumentProps {
 
 export async function fetchDocuments ({ userId }: DocumentProps) {
   try {
-    const URL = 'http://localhost:3000/api/documents/6566b9b1a7b227c15e69a8db/all'
-    console.log(userId)
+    const URL = `http://localhost:3000/api/documents/${userId}/all`
     const response = await fetch(URL)
-    return await response.json() as Document[]
+    return await response.json() as DocumentProps[]
   } catch (error: unknown) {
     throw new Error((error as Error).message)
   }

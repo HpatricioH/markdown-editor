ffaf6ec6ca0fc8f99f39ba87f71ac89d7c4382ad
HPHuerta <hhernanp@gmail.com>
2024-01-30 23:13:03 -0500

    Add user session check in Page component

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -3,9 +3,18 @@
 import React from 'react'
 import LayoutHolder from '../components/Layout/LayoutHolder'
 import Header from '../components/Header/Header'
+import { redirect } from 'next/navigation'
 import MarkDownEditor from '../components/MarkDownEditor/MarkDownEditor'
+import { useSession } from 'next-auth/react'
 
 export default function Page () {
+  // Get user session
+  const { data: session } = useSession()
+
+  if (!session) {
+    redirect('/')
+  }
+
   return (
     <>
       <LayoutHolder />

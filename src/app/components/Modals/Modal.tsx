d1026fe134ac3d90611ada7195bf3269d3034411
HPHuerta <hhernanp@gmail.com>
2024-02-02 22:08:25 -0500

    Update Modal component styles

--- src/app/components/Modals/Modal.tsx
+++ src/app/components/Modals/Modal.tsx
@@ -10,7 +10,7 @@ export default function Modal ({ children, handleClose, modalId }: ModalProps) {
   return (
     <section
       id={modalId}
-      className={`${robotoSlab.className} bg-[#c1c4cb3a] p-4 z-20 fixed inset-0 bg-opacity-60 flex justify-center items-center transition duration-700 ease-in-out  `}
+      className={`${robotoSlab.className} bg-[#c1c4cb3a] p-4 z-20 fixed inset-0 bg-opacity-60 flex justify-center items-center transition duration-700 ease-in-out`}
       onClick={(e: React.MouseEvent<HTMLDivElement>) => { handleClose(e) }}>
       <div
         className='bg-[#FFF] dark:bg-dark-gray-3 rounded-md flex flex-col gap-6 absolute w-[18rem] top-[4.7rem] p-4 md:w-[23.9rem] md:p-7 md:top-[17rem]'>
